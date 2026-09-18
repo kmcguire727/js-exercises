6a34b71d6e79bf349efc23a7a91e188e55830494
Kenneth McGuire <kmcguire727@gmail.com>
2026-09-18 15:05:02 -0400

    Loops and Arrays exercise 3 complete

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, firstItemToRemove, secondItemToRemove, thirdItemToRemove, fourthItemToRemove) {
+    let numArgs = arguments.length - 1;
+
+    for (let i = 1; i <= numArgs; i++) {
+        do {
+            let indexOfItem = array.indexOf(arguments[i]);
+            if (indexOfItem === -1) {
+                break;
+            }
+            array.splice(indexOfItem, 1);
+        } while (array.includes(arguments[i]));
+    }
+    return array;
 };
 
 // Do not edit below this line
