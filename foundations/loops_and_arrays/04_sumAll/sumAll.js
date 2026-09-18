93cd28226fb602e6596058dfa3ec939d3634a238
Kenneth McGuire <kmcguire727@gmail.com>
2026-09-18 15:22:12 -0400

    Loops and Arrays Exercise 4 complete

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let output = 0;
 
-};
+    if ((Number.isInteger(start) && start > 0) && (Number.isInteger(end) && end > 0)) {
+        if (start < end) {
+            for (let i = start; i <= end; i++) {
+                output += i;
+            }
+        } else {
+            for (let i = end; i <= start; i++) {
+                output += i;
+            }
+        }
+        return output;
+    } else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
