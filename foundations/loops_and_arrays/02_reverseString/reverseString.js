fc44106c8a2787c8442a74fa4e860d9cb9f96654
Kenneth McGuire <kmcguire727@gmail.com>
2026-09-18 14:26:57 -0400

    Loops and Arrays Exerc. 02 complete

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let output = "";
+    let lastIndex = string.length - 1;
 
+    for (let i = lastIndex; i >= 0; i--) {
+        output += string[i];
+    }
+
+    return output;
 };
 
 // Do not edit below this line
