6441533a639c4d4444c07696d97f779edc7ffbaa
Kenneth McGuire <kmcguire727@gmail.com>
2026-09-18 15:48:29 -0400

    Loops and arrays exercise 06 complete

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  /*  Subtract 32 from degrees Fahrenheit
+      Divide the result by 1.8 (or 9/5)
+  */
+  let output = (tempFahr - 32) / 1.8;
+  return Math.round(output * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  /*  Multiply degrees Celsius by 1.8 (or 9/5)
+      Add 32 to the product from step 1
+  */
+  let output = (tempCel * 1.8) + 32;
+  return Math.round(output * 10) / 10;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
